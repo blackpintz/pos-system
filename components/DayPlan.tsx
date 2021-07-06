7726c319324102c18ec83a6084fbc714a16ac405
blackpintz <rosewanjohi24@gmail.com>
2021-07-06 16:10:20 +0300

    finished the weekly-plan page.

--- components/DayPlan.tsx
+++ components/DayPlan.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Container, Typography, Button, Box} from '@material-ui/core';
+import {Container, Typography, Button, Box, Divider} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useForm } from 'react-hook-form';
+import {useDispatch} from 'react-redux';
+import {addTaskToDB} from '../actions/todos'
 
 type formValues = {
     taskOne: string,
@@ -13,7 +15,7 @@ type formValues = {
 
 const useStyles = makeStyles(() => ({
     container: {
-        padding: "2rem",
+        padding: "0.5rem 2rem",
         backgroundColor: "#fff",
         color: "#808080",
         "& input" : {
@@ -31,46 +33,67 @@ const useStyles = makeStyles(() => ({
     button: {
         marginTop: "1rem",
         width: "20%"
+    },
+    divider: {
+        height: "3px",
+        margin: "0.5rem 0"
+    },
+    title: {
+        paddingLeft: "24px"
     }
   }))
 
-const DayPlan = () => {
+const DayPlan = (props: any) => {
     const classes = useStyles();
+    const dispatch = useDispatch()
     const {register, handleSubmit, reset} = useForm<formValues>();
+    const {day, title} = props
+
+    const handlingData = (task: string) => {
+        if(task !== "") {
+            const taskData = {
+                task,
+                completed: false,
+                created_at: new Date().toISOString()
+            }
+            dispatch(addTaskToDB(day, taskData))
+        }
+    }
 
     const onSubmit = (data: any) => {
-        console.log(data);
+        const {taskOne, taskTwo, taskThree, taskFour, taskFive} = data
+        handlingData(taskOne);
+        handlingData(taskTwo);
+        handlingData(taskThree);
+        handlingData(taskFour);
+        handlingData(taskFive);
         reset();
+    }
+
+    const InputBox = (props: any) => {
+        const {label, num} = props
+        return (
+            <Box pl={3}>
+                <label>{num}</label>
+                <input {...register(label)} />
+            </Box>
+        )
     }
 
     return (
         <Container className={classes.container} maxWidth="sm">
-             <Typography variant="h6">Monday</Typography>
+             <Typography className={classes.title} variant="h6">{title}</Typography>
              <form onSubmit={handleSubmit(onSubmit)}>
-                 <Box>
-                    <label>1.</label>
-                    <input {...register("taskOne")} />
-                 </Box>
-                 <Box>
-                    <label>2.</label>
-                    <input {...register("taskTwo")} />
-                 </Box>
-                 <Box>
-                     <label>3.</label>
-                     <input {...register("taskThree")} />
-                 </Box>
-                 <Box>
-                     <label>4.</label>
-                     <input {...register("taskFour")} />
-                 </Box>
-                 <Box>
-                     <label>5.</label>
-                     <input {...register("taskFive")} />
-                 </Box>
+                 <InputBox label="taskOne" num="1." />
+                 <InputBox label="taskTwo" num="2." />
+                 <InputBox label="taskThree" num="3." />
+                 <InputBox label="taskFour" num="4." />
+                 <InputBox label="taskFive" num="5." />
                  <Box display="flex" justifyContent="flex-end" width="84%">
                   <Button className={classes.button} variant="contained" color="secondary" type="submit">Add</Button>
                  </Box>
              </form>
+             <Divider className={classes.divider} />
         </Container>
     )
 }
--- pages/weekly-plan.tsx
+++ pages/weekly-plan.tsx
@@ -1,11 +1,23 @@
 import DayPlan from "../components/DayPlan";
 import {Typography} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    title: {
+        marginTop: "1rem"
+    }
+}))
 
 const WeeklyPlan = () => {
+    const classes = useStyles();
     return (
         <>
-            <Typography align="center" variant="h5">Weekly Plan</Typography>
-            <DayPlan />
+            <Typography className={classes.title} align="center" variant="h5">Weekly Plan</Typography>
+            <DayPlan day="MondayTodos" title="Monday" />
+            <DayPlan day="TuesdayTodos" title="Tuesday" />
+            <DayPlan day="WednesdayTodos" title="Wednesday" />
+            <DayPlan day="ThursdayTodos" title="Thursday" />
+            <DayPlan day="FridayTodos" title="Friday" />
         </>
     )
 }
