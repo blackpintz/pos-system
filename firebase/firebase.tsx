89b6d7fb22a64eb2a9c2bff5805ecb534da19eb7
blackpintz <rosewanjohi24@gmail.com>
2021-12-18 15:30:24 +0300

    fix webpack errors.

--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -1,12 +1,15 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
 
+require('dotenv').config()
+console.log()
+
 const config = {
-  apiKey: "AIzaSyALhzzEHt8k_aHl1ARc24EuDLNxwtRZ0-k",
-  authDomain: "rose-3bae9.firebaseapp.com",
-  databaseURL: "https://rose-3bae9-default-rtdb.firebaseio.com",
-  projectId: "rose-3bae9",
-  storageBucket: "rose-3bae9.appspot.com",
+  apiKey: process.env.NEXT_PUBLIC_api_key,
+  authDomain: process.env.NEXT_PUBLIC_domain,
+  databaseURL: process.env.NEXT_PUBLIC_dbURL,
+  projectId: process.env.NEXT_PUBLIC_id,
+  storageBucket: process.env.NEXT_PUBLIC_bucket,
   messagingSenderId: "508637613984",
   appId: "1:508637613984:web:36c2b16bb9f7ea30c45ec5",
   measurementId: "G-FW2CBH7SVV"
--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-module.exports = { target: 'serverless' };
+module.exports = {
+    webpack(config) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+          // by next.js will be dropped. Doesn't make much sense, but how it is
+        fs: false, // the solution
+      };
+  
+      return config;
+    },
+  };
